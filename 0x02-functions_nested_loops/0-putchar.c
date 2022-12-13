e0b59ff9199dcdca2b56650c2a80d1daab020282
ziontsion <tsionnew@gmail.com>
2022-12-12 22:42:32 -0800

    print text

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "_putchar \n"
+(* Return: Always 0 (success)
+**/
+
+int main(void)
+{
+	puts("_putchar \n");
+
+return (0);
+}
