ebf35f29790cc2245e887c8b9aaf4d11dd44b825
ziontsion <tsionnew@gmail.com>
2022-12-14 04:30:06 -0800

    positve or negative_number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Test function for positive or negative
+ *
+ * Description : return positve or negative
+ *
+ * Return: Success return 0.
+*/
+
+int main(void)
+{
+	int y;
+
+	y = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
