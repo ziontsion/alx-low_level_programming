007f963b6d415da6a7c9affd3f07ac14401c0297
ziontsion <tsionnew@gmail.com>
2023-01-09 07:00:49 -0800

    malloc str

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  * create_array - Creates an array of chars and initializes it with a
+ *  * specific char
+ *  *
+ *  * @size: size of the array
+ *  * @c: the character to initialize the array with
+ *    *
+ *    * Return: a pointer to the array, or NULL if it fails
+*/
